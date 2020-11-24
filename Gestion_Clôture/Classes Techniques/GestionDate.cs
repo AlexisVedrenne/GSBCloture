2a94750ea126ab827cbf7f95f7d7329f6fe4fb80
alexisvedrenne482@gmail.com <alexisvedrenne482@gmail.com>
2020-11-24 17:38:15 +0100

    Création de la classe Gestion Date

--- Gestion_Clôture/Classes Techniques/GestionDate.cs
+++ Gestion_Clôture/Classes Techniques/GestionDate.cs
@@ -6,8 +6,99 @@ using System.Threading.Tasks;
 
 namespace Classe.Technique
 {
-    class GestionDate
+    abstract class GestionDate
     {
+        /// <summary>
+        /// Fonction qui permet de récuperer le mois precedent d'une date passer en paramettre
+        /// </summary>
+        /// <param name="date">La date à passer en parametre</param>
+        /// <returns>Retourne le mois precedent cette date</returns>
+        public static string getMoisPrecedent(DateTime date)
+        {
+            if ((date.Month - 1) < 10)
+            {
+                return "0" + (date.Month - 1).ToString();
+            }
+            else if ((date.Month - 1) == 0)
+            {
+                return "12";
+            }
+            else
+            {
+                return (date.Month - 1).ToString();
+            }
+        }
+        /// <summary>
+        /// Fonction qui permet de récuperer le mois precedent de la date courrante
+        /// </summary>
+        /// <returns>Retourne le mois precedent la date courrant </returns>
+        public static string getMoisPrecedent()
+        {
+            return getMoisPrecedent(DateTime.Now);
+        }
+        /// <summary>
+        /// Fonction qui permet de récuperer le mois suivant de la date passer en parametre
+        /// </summary>
+        /// <param name="date">La date passée en parametre</param>
+        /// <returns>Retourne le mois suivant</returns>
+        public static string getMoisSuivant(DateTime date)
+        {
+            if ((date.Month + 1) < 10)
+            {
+                return "0" + (date.Month + 1).ToString();
+            }
+            else if ((date.Month + 1) == 13)
+            {
+                return "01";
+            }
+            else
+            {
+                return (date.Month + 1).ToString();
+            }
+        }
+        /// <summary>
+        /// Foncton qui permet de récuperer le mois de la date courrante
+        /// </summary>
+        /// <returns>Retourne la mois suivant la date courrante</returns>
+        public static string getMoisSuivant()
+        {
+            return getMoisSuivant(DateTime.Now);
+        }
+
+        /// <summary>
+        /// Fonction permet de savoir si la date passer en parametre est dans l'intervalle
+        /// </summary>
+        /// <param name="numJour1">L numéro du jour</param>
+        /// <param name="numJour2">Le numéro du jour</param>
+        /// <param name="date">La date</param>
+        /// <returns>Retourne vrai si la date est dans l'interval</returns>
+        public static bool Entre(int numJour1, int numJour2, DateTime date)
+        {
+            try
+            {
+                if ((numJour1 < 31 && numJour2 < 31) && numJour1 < numJour2)
+                {
+                    return numJour1 < date.Day && date.Day < numJour2;
+                }
+                else if (numJour2 < numJour1)
+                {
+                    return numJour2 < date.Day && date.Day < numJour1;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erreur : " + ex.Message);
+            }
+        }
+        public static bool Entre(int numJour1,int numJour2)
+        {
+           return Entre(numJour1, numJour2, DateTime.Now);
+        }
+        
 
     }
 }
--- Gestion_Clôture/Program.cs
+++ Gestion_Clôture/Program.cs
@@ -14,7 +14,17 @@ namespace Gestion_Clôture
             //BddMySql.GetInstance();
             //int nb = BddMySql.ClotureFraisDuMois("202011");
             //Console.WriteLine(nb);
-            
+            DateTime d1 = new DateTime(1991, 10, 12);
+            DateTime d2 = new DateTime(1991, 11, 9);
+            DateTime d3 = new DateTime(2001, 1, 5);
+            Console.WriteLine("Mois precedent : "+GestionDate.getMoisPrecedent());
+            Console.WriteLine("Mois suivant : "+GestionDate.getMoisSuivant());
+            Console.WriteLine("Mois precedent param : "+GestionDate.getMoisPrecedent(d1));
+            Console.WriteLine("Mois suivant param : " + GestionDate.getMoisSuivant(d2));
+            Console.WriteLine("Entre vrai : "+GestionDate.Entre(25,20));
+            Console.WriteLine("Entre faux : "+GestionDate.Entre(2,10));
+            Console.WriteLine("Entre param vrai : " + GestionDate.Entre(1,6 ,d3));
+            Console.WriteLine("Entre param faux : " + GestionDate.Entre(20, 30,d3));
             Console.ReadLine();
         }
     }
