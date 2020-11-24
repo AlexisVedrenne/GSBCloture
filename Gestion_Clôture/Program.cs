e25c7de246b75bd82cd3b724de36f1620a7f9733
alexisvedrenne482@gmail.com <alexisvedrenne482@gmail.com>
2020-11-24 15:34:53 +0100

    Création de la classe d'accés au donnée

---
+++ Gestion_Clôture/Program.cs
@@ -0,0 +1,21 @@
+﻿using Classe.Technique;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Clôture
+{
+    class Program
+    {
+        static void Main()
+        {
+            //BddMySql.GetInstance();
+            //int nb = BddMySql.ClotureFraisDuMois("202011");
+            //Console.WriteLine(nb);
+            
+            Console.ReadLine();
+        }
+    }
+}
